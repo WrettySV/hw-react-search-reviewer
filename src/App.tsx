a1a1afc52c735cacff0f8dfb5cdb815308b7be3f
Svetlana <svetl.luckina2016@yandex.ru>
2024-02-02 23:07:25 +0300

    react get reviewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Reviewer from "./components/reviewer/Reviewer";
+import logo from './github-logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="GitHub Logo" />
+                <h1 className="App-title">Поиск ревьюера</h1>
+            </header>
+            <Reviewer />
+        </div>
+    );
 }
 
 export default App;
