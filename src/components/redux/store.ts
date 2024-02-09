1ce11d6372a264933eec81546c6918b63d473c88
Svetlana <svetl.luckina2016@yandex.ru>
2024-02-09 14:36:09 +0300

    Updates

--- src/components/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducer from './reducer';
-import {asyncActionsMiddleware} from "./middleware";
-
-const store = createStore(reducer, applyMiddleware(asyncActionsMiddleware));
-
-export type RootState = ReturnType<typeof store.getState>;
-
-export default store;
